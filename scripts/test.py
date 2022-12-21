da2b83ee44ca8f4daabd7ea0782bf623a3a062e1
MuhammadAwon <awon195@gmail.com>
2022-12-22 02:01:52 +0500

    Modify test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,20 +1,13 @@
-import os
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
 
-# # Local host url (with docker image for aws lambda)
-# url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
-
-# # API url
-# api_url = os.getenv('API_URL')
-
+# Local host url (with docker image)
+url = 'http://localhost:9696/predict'
 
 # Image data to send with POST request
-data = {'url': 'https://target.scene7.com/is/image/Target/GUEST_10f0fb6f-05d7-4a1d-9cae-2fa12a691741?wid=800&hei=800&qlt=80&fmt=pjpeg'}
+data = {'url': 'https://m.media-amazon.com/images/I/61b0ng0Gg5L._AC_UL320_.jpg'}
+
 
 # Parse POST request of the data to JSON
-result = requests.post(api_url, json=data).json()
+result = requests.post(url, json=data).json()
 print(result)
-
